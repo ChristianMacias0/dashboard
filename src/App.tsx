254d77c22221fd8f9a9256ecae390cb09ddc6226
Christian Macias <cjavmaci@espol.edu.ec>
2025-06-25 14:52:30 +0000

    aveance en clase

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Grid } from '@mui/material';
+import { Grid, ThemeProvider, createTheme } from '@mui/material';
+import SelectorUI from './components/SelectorUI';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <ThemeProvider theme={darkTheme}>
       <Grid container spacing={5} justifyContent="center" alignItems="center">
 
          {/* Encabezado */}
@@ -18,7 +25,7 @@ function App() {
          <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
 
          {/* Selector */}
-         <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
+         <Grid size={{ xs: 12, md: 3 }}><SelectorUI Elemento: Selector /></Grid>
 
          {/* Indicadores */}
          <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
@@ -36,27 +43,7 @@ function App() {
       <div>
         <h2>Bienvenitue to the dashboard</h2>
       </div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </ThemeProvider>
   )
 }
 
